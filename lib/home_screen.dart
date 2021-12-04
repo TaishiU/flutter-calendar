e6b521b87035bcc9e853ab71820de78fdda5cc86
TaishiU <nbadreamteam3@gmail.com>
2021-12-04 16:28:07 +0900

    カレンダー表示

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,15 +9,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('HomeScreen'),
+        title: Text('Calender'),
       ),
       body: Container(
-        child: Text('Home'),
+        child: TableCalendar(
+          firstDay: DateTime.utc(2020, 1, 1),
+          lastDay: DateTime.utc(2030, 12, 31),
+          focusedDay: _focusedDay,
+          calendarFormat: _calendarFormat,
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+          ),
+          onFormatChanged: (CalendarFormat format) {
+            if (_calendarFormat != format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            }
+          },
+          /*どの日付が現在選択されているか判断*/
+          selectedDayPredicate: (DateTime day) {
+            /*bool値を返す*/
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+            if (!isSameDay(_selectedDay, selectedDay)) {
+              setState(() {
+                _selectedDay = selectedDay; /*選択された日付*/
+                _focusedDay = focusedDay; /*現在の日付*/
+              });
+            }
+          },
+        ),
       ),
     );
   }
