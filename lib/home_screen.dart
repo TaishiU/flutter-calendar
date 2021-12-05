0b6f4606b45beff4ea782c9181173cdd9f2a201c
TaishiU <nbadreamteam3@gmail.com>
2021-12-05 17:31:07 +0900

    [fix] UI修正

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   DateTime _focusedDay = DateTime.now();
+  String _displayDate = '今日';
   DateTime? _selectedDay;
   CalendarFormat _calendarFormat = CalendarFormat.month;
 
@@ -18,38 +20,77 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Calender'),
-      ),
-      body: Container(
-        child: TableCalendar(
-          firstDay: DateTime.utc(2020, 1, 1),
-          lastDay: DateTime.utc(2030, 12, 31),
-          focusedDay: _focusedDay,
-          calendarFormat: _calendarFormat,
-          headerStyle: HeaderStyle(
-            formatButtonVisible: false,
-          ),
-          onFormatChanged: (CalendarFormat format) {
-            if (_calendarFormat != format) {
-              setState(() {
-                _calendarFormat = format;
-              });
-            }
-          },
-          /*どの日付が現在選択されているか判断*/
-          selectedDayPredicate: (DateTime day) {
-            /*bool値を返す*/
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-            if (!isSameDay(_selectedDay, selectedDay)) {
-              setState(() {
-                _selectedDay = selectedDay; /*選択された日付*/
-                _focusedDay = focusedDay; /*現在の日付*/
-              });
-            }
-          },
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(_displayDate),
+            Icon(
+              Icons.arrow_drop_up,
+              size: 25,
+            ),
+          ],
         ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            child: TableCalendar(
+              locale: 'ja_JP',
+              firstDay: DateTime.utc(2020, 1, 1),
+              lastDay: DateTime.utc(2030, 12, 31),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              headerStyle: HeaderStyle(
+                titleCentered: true,
+                formatButtonVisible: false,
+              ),
+              calendarStyle: CalendarStyle(
+                selectedDecoration: BoxDecoration(
+                  color: Colors.orange,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              onFormatChanged: (CalendarFormat format) {
+                if (_calendarFormat != format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              /*どの日付が現在選択されているか判断*/
+              selectedDayPredicate: (DateTime day) {
+                /*bool値を返す*/
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+                if (!isSameDay(_selectedDay, selectedDay)) {
+                  setState(() {
+                    _selectedDay = selectedDay; /*選択された日付*/
+                    _focusedDay = focusedDay; /*現在の日付*/
+
+                    // if (selectedDay.difference(DateTime.now()).inDays == 0) {
+                    if (isSameDay(selectedDay, DateTime.now())) {
+                      _displayDate = '今日';
+                    } else {
+                      _displayDate = DateFormat.MMMEd('ja').format(selectedDay);
+                    }
+                  });
+                }
+              },
+            ),
+          ),
+          SizedBox(height: 60),
+          Container(
+            color: Colors.red,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text('Home'),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:calender/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
+  initializeDateFormatting('ja');
   runApp(const MyApp());
 }
 
