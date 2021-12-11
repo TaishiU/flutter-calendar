4ae1d36b694616ebd314b8b235c3aeb07ba0000a
TaishiU <nbadreamteam3@gmail.com>
2021-12-11 20:16:01 +0900

    [fix] UI修正

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 void main() {
@@ -274,6 +275,31 @@ class _TableRangeExampleState extends State<TableRangeExample> {
   DateTime? _selectedDay;
   DateTime? _rangeStart;
   DateTime? _rangeEnd;
+  String startYear = '';
+  String startDay = '';
+  String endYear = '';
+  String endDay = '';
+  bool switchValue = false;
+
+  void startAtDate(DateTime? dateTime) {
+    // startAtTextController.text = DateFormat('yyyy年MM月dd日E曜日', 'ja_JP').format(dateTime)
+    //startAtTextController.text = DateFormat.MMMEd('ja').format(dateTime);
+    //state = state.copyWith(startAt: dateTime);
+    // state = state.copyWith(startYear: startYear);
+    // state = state.copyWith(startDay: startDay);
+    startYear = DateFormat('yyyy年', 'ja_JP').format(dateTime!);
+    startDay = DateFormat.MMMEd('ja').format(dateTime);
+  }
+
+  void endAtDate(DateTime? dateTime) {
+    // endAtDateTextController.text = DateFormat('yyyy年MM月dd日E曜日', 'ja_JP').format(dateTime);
+    // endAtDateTextController.text = DateFormat.MMMEd('ja').format(dateTime);
+    // state = state.copyWith(endAt: dateTime);
+    // state = state.copyWith(endYear: endYear);
+    // state = state.copyWith(endDay: endDay);
+    endYear = DateFormat('yyyy年', 'ja_JP').format(dateTime!);
+    endDay = DateFormat.MMMEd('ja').format(dateTime);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -281,94 +307,253 @@ class _TableRangeExampleState extends State<TableRangeExample> {
       appBar: AppBar(
         title: Text('TableCalendar - Range'),
       ),
-      body: TableCalendar(
-        locale: 'ja_JP',
-        firstDay: DateTime.utc(2020, 1, 1),
-        lastDay: DateTime.utc(2030, 12, 31),
-        focusedDay: _focusedDay,
-        startingDayOfWeek: StartingDayOfWeek.monday,
-        headerStyle: HeaderStyle(
-          titleCentered: true,
-          formatButtonVisible: false,
-          leftChevronIcon: Icon(
-            Icons.chevron_left,
-            color: Colors.grey,
-            size: 35,
-          ),
-          rightChevronIcon: Icon(
-            Icons.chevron_right,
-            color: Colors.grey,
-            size: 35,
-          ),
-          headerPadding: EdgeInsets.only(top: 10, bottom: 15),
-          leftChevronPadding: EdgeInsets.only(left: 0),
-          rightChevronPadding: EdgeInsets.only(right: 0),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.date_range),
+                    SizedBox(width: 10),
+                    Text(
+                      '期間',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                Switch(
+                  value: switchValue,
+                  onChanged: (value) {
+                    setState(() {
+                      switchValue = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            if (!switchValue) SizedBox.shrink(),
+            if (!switchValue && (startDay != '' && endDay != ''))
+              Column(
+                children: [
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        startDay,
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                      Container(
+                        width: 40,
+                        child: Center(child: Text('〜')),
+                      ),
+                      Text(
+                        endDay,
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            if (switchValue)
+              Column(
+                children: [
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        height: 55,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        // child: TextFormField(
+                        //   controller: controller.startAtTextController,
+                        //   onTap: () {
+                        //     controller.updateCurrentFocused(0);
+                        //   },
+                        //   style: TextStyle(fontSize: 14),
+                        // ),
+                        // child: GestureDetector(
+                        //   onTap: () => controller.updateCurrentFocused(0),
+                        //   child: Center(
+                        //     child: Text(state.startDay),
+                        //   ),
+                        // ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              startYear,
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              startDay,
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 40,
+                        child: Center(child: Text('〜')),
+                      ),
+                      Container(
+                        height: 55,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        // child: TextFormField(
+                        //   controller: controller.endAtDateTextController,
+                        //   onTap: () => controller.updateCurrentFocused(1),
+                        //   style: TextStyle(fontSize: 14),
+                        // ),
+                        // child: GestureDetector(
+                        //   onTap: () => controller.updateCurrentFocused(1),
+                        //   child: Center(
+                        //     child: Text(state.endDay),
+                        //   ),
+                        // ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              endYear,
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              endDay,
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  TableCalendar(
+                    locale: 'ja_JP',
+                    firstDay: DateTime.utc(2020, 1, 1),
+                    lastDay: DateTime.utc(2030, 12, 31),
+                    focusedDay: _focusedDay,
+                    startingDayOfWeek: StartingDayOfWeek.monday,
+                    headerStyle: HeaderStyle(
+                      titleCentered: true,
+                      formatButtonVisible: false,
+                      leftChevronIcon: Icon(
+                        Icons.chevron_left,
+                        color: Colors.grey.shade400,
+                        size: 35,
+                      ),
+                      rightChevronIcon: Icon(
+                        Icons.chevron_right,
+                        color: Colors.grey.shade400,
+                        size: 35,
+                      ),
+                      headerPadding: EdgeInsets.only(top: 0, bottom: 5),
+                      leftChevronPadding: EdgeInsets.only(left: 0),
+                      rightChevronPadding: EdgeInsets.only(right: 0),
+                    ),
+                    calendarStyle: CalendarStyle(
+                      cellMargin: EdgeInsets.all(1),
+                      defaultDecoration:
+                          BoxDecoration(color: Colors.grey.shade200),
+                      todayTextStyle: TextStyle(color: Colors.black),
+                      todayDecoration: BoxDecoration(
+                        color: Colors.yellow,
+                      ),
+                      selectedDecoration: BoxDecoration(
+                        color: Colors.orange,
+                        shape: BoxShape.circle,
+                      ),
+                      weekendTextStyle: TextStyle(color: Colors.black),
+                      weekendDecoration:
+                          BoxDecoration(color: Colors.grey.shade200),
+                      outsideTextStyle: TextStyle(color: Colors.black),
+                      outsideDecoration:
+                          BoxDecoration(color: Colors.grey.shade200),
+                      rangeHighlightColor: Colors.orange.shade100,
+                      rangeStartDecoration: BoxDecoration(
+                        color: Colors.orange,
+                      ),
+                      rangeEndDecoration: BoxDecoration(
+                        color: Colors.orange,
+                      ),
+                    ),
+                    daysOfWeekHeight: 25,
+                    daysOfWeekStyle: DaysOfWeekStyle(
+                      weekdayStyle: TextStyle(color: Colors.grey),
+                      weekendStyle: TextStyle(color: Colors.grey),
+                    ),
+                    selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                    rangeStartDay: _rangeStart,
+                    rangeEndDay: _rangeEnd,
+                    calendarFormat: _calendarFormat,
+                    rangeSelectionMode: _rangeSelectionMode,
+                    onDaySelected: (selectedDay, focusedDay) {
+                      if (!isSameDay(_selectedDay, selectedDay)) {
+                        setState(() {
+                          _selectedDay = selectedDay;
+                          _focusedDay = focusedDay;
+                          _rangeStart = null; // Important to clean those
+                          _rangeEnd = null;
+                          _rangeSelectionMode = RangeSelectionMode.toggledOff;
+                        });
+                      }
+                    },
+                    onRangeSelected: (start, end, focusedDay) {
+                      setState(() {
+                        _selectedDay = null;
+                        _focusedDay = focusedDay;
+                        _rangeStart = start;
+                        _rangeEnd = end;
+                        _rangeSelectionMode = RangeSelectionMode.toggledOn;
+                      });
+                      startAtDate(start);
+                      endAtDate(end);
+                    },
+                    onFormatChanged: (format) {
+                      if (_calendarFormat != format) {
+                        setState(() {
+                          _calendarFormat = format;
+                        });
+                      }
+                    },
+                    onPageChanged: (focusedDay) {
+                      _focusedDay = focusedDay;
+                    },
+                  ),
+                ],
+              ),
+          ],
         ),
-        calendarStyle: CalendarStyle(
-          cellMargin: EdgeInsets.all(1),
-          defaultDecoration: BoxDecoration(color: Colors.grey.shade300),
-          todayTextStyle: TextStyle(color: Colors.black),
-          todayDecoration: BoxDecoration(
-            color: Colors.yellow,
-          ),
-          selectedDecoration: BoxDecoration(
-            color: Colors.orange,
-            shape: BoxShape.circle,
-          ),
-          weekendTextStyle: TextStyle(color: Colors.black),
-          weekendDecoration: BoxDecoration(color: Colors.grey.shade300),
-          outsideTextStyle: TextStyle(color: Colors.black),
-          outsideDecoration: BoxDecoration(color: Colors.grey.shade300),
-          rangeHighlightColor: Colors.orange.shade100,
-          rangeStartDecoration: BoxDecoration(
-            color: Colors.orange,
-          ),
-          rangeEndDecoration: BoxDecoration(
-            color: Colors.orange,
-          ),
-        ),
-        daysOfWeekHeight: 20.0,
-        daysOfWeekStyle: DaysOfWeekStyle(
-          weekdayStyle: TextStyle(color: Colors.grey),
-          weekendStyle: TextStyle(color: Colors.grey),
-        ),
-        selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-        rangeStartDay: _rangeStart,
-        rangeEndDay: _rangeEnd,
-        calendarFormat: _calendarFormat,
-        rangeSelectionMode: _rangeSelectionMode,
-        onDaySelected: (selectedDay, focusedDay) {
-          if (!isSameDay(_selectedDay, selectedDay)) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay;
-              _rangeStart = null; // Important to clean those
-              _rangeEnd = null;
-              _rangeSelectionMode = RangeSelectionMode.toggledOff;
-            });
-          }
-        },
-        onRangeSelected: (start, end, focusedDay) {
-          setState(() {
-            _selectedDay = null;
-            _focusedDay = focusedDay;
-            _rangeStart = start;
-            _rangeEnd = end;
-            _rangeSelectionMode = RangeSelectionMode.toggledOn;
-            print('_rangeStart: $_rangeStart');
-            print('_rangeEnd: $_rangeEnd');
-          });
-        },
-        onFormatChanged: (format) {
-          if (_calendarFormat != format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          }
-        },
-        onPageChanged: (focusedDay) {
-          _focusedDay = focusedDay;
-        },
       ),
     );
   }
